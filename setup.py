bd8de62b3ff3f7ca96dd021874a960436ab9e2ad
Phil Wang <lucidrains@gmail.com>
2022-05-06 07:31:16 -0700

    0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'CoCa-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'CoCa, Contrastive Captioners are Image-Text Foundation Models - Pytorch',
   author = 'Phil Wang',
